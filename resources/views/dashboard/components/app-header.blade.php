1fa76e6267709b5c8fcdcf970ba7cde2e96c7f69
eslam <elbadry693@gmail.com>
2024-07-06 01:19:08 +0300

    make link to mail

--- resources/views/dashboard/components/app-header.blade.php
+++ resources/views/dashboard/components/app-header.blade.php
@@ -40,11 +40,11 @@
 										</li>
 
 										<li class="dropdown main-profile-menu nav nav-item nav-link ps-lg-2">
-											<a class="new nav-link profile-user d-flex" href="" data-bs-toggle="dropdown"><img alt="" src="{{ asset(Auth::user()->profileImage) }}" class=""></a>
+											<a class="new nav-link profile-user d-flex" href="" data-bs-toggle="dropdown"><img alt="" src="#" class=""></a>
 											<div class="dropdown-menu">
 												<div class="menu-header-content p-3 border-bottom">
 													<div class="d-flex wd-100p">
-														<div class="main-img-user"><img alt="" src="{{ asset(Auth::user()->profileImage) }}" class=""></div>
+														<div class="main-img-user"><img alt="" src="#" class=""></div>
 														<div class="ms-3 my-auto">
 															<h6 class="tx-15 font-weight-semibold mb-0">{{auth()->user()->name}}</h6>
 														</div>
